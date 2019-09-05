f071a214895b914b1845030c43d95a1b46f579b3
cdac <cdac@10.226.41.152>
2019-09-05 16:55:59 -0700

    Controller Class

--- demomvc/src/main/resources/com/ani/AddController.java
+++ demomvc/src/main/resources/com/ani/AddController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.portlet.ModelAndView;
 
 import demomvc.service.Service;
-@Component
+
 @Controller
 public class AddController {
 	@RequestMapping("/add")
--- demomvc/target/classes/com/ani/AddController.java
+++ demomvc/target/classes/com/ani/AddController.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.portlet.ModelAndView;
-@Component
+
+import demomvc.service.Service;
+
 @Controller
 public class AddController {
 	@RequestMapping("/add")
 	public ModelAndView add(HttpServletRequest req, HttpServletResponse res) {
 		int i = Integer.parseInt(req.getParameter("t1"));
 		int j = Integer.parseInt(res.getParameter("t2"));
-		int k= i+j;
-		
+		Service as = new Service();
+		int k = as.add(i, j);
 		ModelAndView mv = new ModelAndView();
 		mv.setView("Display.jsp);"
 		mv.addObject("result",k);
